a48e706a35d0e2207d2d016e226c9e74aa02ed47
Benjamin <bcastilloa@it.ucsc.cl>
2025-10-23 19:52:50 -0300

    Actualizacion index.js, crud usuarios listo.

--- index.js
+++ index.js
@@ -9,3 +9,54 @@ const config = {
     }
 
     const pool = new Pool(config);
+
+    const getusr = async()=>{
+
+    try{
+        const res= await pool.query('SELECT * FROM tab_usr')
+        console.log(res.rows)
+        pool.end();
+    }catch(e){
+        console.log(e)
+    }
+};
+
+const insertusr = async()=>{
+
+    try{
+        const text =('Insert into tab_usr(tus_nom, tus_mail) VALUES($1, $2) RETURNING *')
+        const values =['Usuario10','test@test.com']
+        
+        const res = await pool.query(text,values)
+        console.log(res.rows)
+        pool.end();
+    }catch(e){
+        console.log(e)
+    }
+};
+
+
+const deleteusr = async()=>{
+
+  
+        const text =('DELETE FROM tab_usr WHERE tus_id_usr=$1')
+        const values =['6']
+        
+        const res = await pool.query(text,values)
+        console.log(res);
+};
+
+const editusr = async()=>{
+
+  
+        const text =('UPDATE tab_usr SET tus_nom = $1 WHERE tus_id_usr=$2')
+        const values =['JUAN', '3']
+        
+        const res = await pool.query(text,values)
+        console.log(res);
+};
+
+//editusr();
+//deleteusr();
+//insertusr();
+//getusrs();
